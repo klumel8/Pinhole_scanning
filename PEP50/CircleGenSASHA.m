6a127e028f55d5e31827d3799c00dc8ca576f23c
koloneltedi <sashaivlev@hotmail.com>
2017-06-07 17:07:44 +0200

    Update CircleGenSASHA.m

--- PEP50/CircleGenSASHA.m
+++ PEP50/CircleGenSASHA.m
@@ -4,6 +4,7 @@ Nc = 4*max(gridrow,gridcol);
 dg = 2*pi/Nc;
 ngrid = gridcol*gridrow;
 C = zeros(Nc,3,ngrid); %
+Res = zeros(Nc,2,ngrid);
 tic
 i = 0:gridcol-1;
 phim = i*2*pi/gridcol;
@@ -15,4 +16,7 @@ gamma = 1:Nc;
 Circles = cat(4,cat(4,times(times(cos(dg*Gamma),sin(Theta)),cos(Phi))-times(sin(dg*Gamma),sin(Phi)), times(times(cos(dg*Gamma),sin(Theta)),sin(Phi))+times(sin(dg*Gamma),cos(Phi))), times(cos(dg*Gamma),cos(Theta)));
 C = reshape(Circles,[ngrid,Nc,3]);
 C = permute(C,[2,3,1]);
+Res(:,1,:) = acos(C(:,3,:));
+Res(:,2,:) = atan2(C(:,2,:),C(:,1,:));
+Res(:,2,:) = 2*pi*(Res(:,2,:)<0)+Res(:,2,:)-2*pi*(Res(:,2,:)>0);
 toc

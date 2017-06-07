b029a8d88c0338e10578f874caf8a02f1c2378f0
koloneltedi <sashaivlev@hotmail.com>
2017-06-07 15:23:11 +0200

    Add files via upload
    My CircleGen function and two required files with new MATLAB function for multidimensional array multiplication

---
+++ PEP50/CircleGenSASHA.m
@@ -0,0 +1,18 @@
+%function C = CircleGen(gridrow,gridcol)
+%a = 0 %
+Nc = 4*max(gridrow,gridcol);
+dg = 2*pi/Nc;
+ngrid = gridcol*gridrow;
+C = zeros(Nc,3,ngrid); %
+tic
+i = 0:gridcol-1;
+phim = i*2*pi/gridcol;
+j = 0:gridrow-1;
+thetam = j*pi/2/gridrow;
+a = 1:ngrid;
+gamma = 1:Nc;
+[Phi,Theta,Gamma] = meshgrid(phim,thetam,gamma);
+Circles = cat(4,cat(4,times(times(cos(dg*Gamma),sin(Theta)),cos(Phi))-times(sin(dg*Gamma),sin(Phi)), times(times(cos(dg*Gamma),sin(Theta)),sin(Phi))+times(sin(dg*Gamma),cos(Phi))), times(cos(dg*Gamma),cos(Theta)));
+C = reshape(Circles,[ngrid,Nc,3]);
+C = permute(C,[2,3,1]);
+toc

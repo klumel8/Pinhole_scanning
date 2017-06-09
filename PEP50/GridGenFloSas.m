4b309c84ff23dc3db8fe823823e84101e508c1a7
koloneltedi <sashaivlev@hotmail.com>
2017-06-09 15:10:48 +0200

    Update GridGenFloSas.m

--- PEP50/GridGenFloSas.m
+++ PEP50/GridGenFloSas.m
@@ -1,47 +1,57 @@
 clear all
 
-gridrow = 129; %Number of rows
-gridcol = 256; %Number of colms
-halvedrow = (gridrow-1)/2+1;
+gridrow = 201; %Number of rows
+gridcol = 400; %Number of colms
 
-rowleng=pi/halvedrow/2; %Height of a row
-colleng=2.*pi/gridcol; %Width of a col
+rowleng=pi/gridrow; %Height of a row
+colleng=2*pi/gridcol; %Width of a col
 
-Nc = max(halvedrow,gridcol)*5; %Number of steps of gamma
-dg = 2*pi/Nc;  %Stepssize of gamma parameter
-ngrid = gridcol*halvedrow;% gridcol*gridrow; %Number of circles  
-gridcir=zeros(ngrid,gridrow,gridcol); %Set the ngrid circlegrids on false
+numberSteps = max(gridrow,gridcol)*5; %Number of steps of gamma
+dg = 2*pi/numberSteps;  %Stepssize of gamma parameter
+numberCircles = gridcol*((gridrow-1)/2+1);% gridcol*gridrow; %Number of circles  
+gridcir=false(numberCircles,gridrow,gridcol); %Set the ngrid circlegrids on false
 
 i = 0:gridcol-1; 
 phim = i*2*pi/gridcol; %Vector from 0 to 2pi-colleng in  ; so phi is a the row (x)
 
-j = 0:halvedrow-1; %j = 0:gridrow-1; 
+j = 0:(gridrow-1)/2; %j = 0:gridrow-1; 
 thetam = j*pi/2/gridrow; %Vector from 0 to pi- rowleng ; So theta is the collumn (y)
 
-a = 1:ngrid; 
-gamma = 1:Nc; 
+a = 1:numberCircles; 
+gamma = 1:numberSteps; 
 [Phi,Theta,Gamma] = meshgrid(phim,thetam,gamma);
 
-Circles = cat(4,cat(4,times(times(cos(dg*Gamma),sin(Theta)),cos(Phi))-times(sin(dg*Gamma),sin(Phi)), times(times(cos(dg*Gamma),sin(Theta)),sin(Phi))+times(sin(dg*Gamma),cos(Phi))), times(cos(dg*Gamma),cos(Theta)));
-C = reshape(Circles,[ngrid,Nc,3]);
+Circles = cat(4,cat(4,...
+    times(times(cos(dg*Gamma),sin(Theta)),cos(Phi))-times(sin(dg*Gamma),sin(Phi)),...
+    times(times(cos(dg*Gamma),sin(Theta)),sin(Phi))+times(sin(dg*Gamma),cos(Phi))), ...
+    times(cos(dg*Gamma),cos(Theta)));
+C = reshape(Circles,[numberCircles,numberSteps,3]);
 C = permute(C,[2,3,1]);
 Res(:,1,:) = acos(C(:,3,:)); %Theta 
 Res(:,2,:) = atan2(C(:,2,:),C(:,1,:)); %Phi
 Res(:,2,:) = 2*pi*(Res(:,2,:)<0)+Res(:,2,:)-2*pi*(Res(:,2,:)>2*pi);
+%tic
+index(:,1,:) = floor(Res(:,1,:)/rowleng);%Theta
+index(:,2,:) = floor(Res(:,2,:)/colleng);%Phi
 
-index(:,1,:) = floor(Res(:,1,:)/rowleng)+1;%Theta
-index(:,2,:) = floor(Res(:,2,:)/colleng)+1;%Phi
 
-linearIndex = zeros(Nc,1,ngrid);
-linearIndex = halvedrow*index(:,2,:)+index(:,1,:);
+linearIndex = gridrow*index(:,2,:)+index(:,1,:);
+linearIndex = squeeze(linearIndex);
 
-gridcir(a,linearIndex(:))
+labels= numberCircles*meshgrid(a-1,gamma);
+linearIndex = linearIndex+labels;
+linearIndex = reshape(linearIndex,[numberSteps*numberCircles,1]);
+
+
+
+gridcir(linearIndex(:,1))=1;
+%toc
+
+%weight=zeros(ngrid,1);
 %{
-weight=zeros(ngrid,1);
 tic
 grid = zeros(gridrow,gridcol);
 for i = 1:length(index(:,:,1))
-% NOTE NOTE NOTE zou dit niet |grid(index(i,1,j),index(i,2,j)) =1;| moeten zijn waarin we ook nog eens door j loopen voor elk punt(1 tot Nc)?
     grid(index(i,1,1),index(i,2,1)) =1;
 end
 toc

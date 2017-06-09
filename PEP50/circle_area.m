52e965c5124eafd951beb40d48ef24fcb39f79ef
klumel8 <floris.brulleman@gmail.com>
2017-06-09 20:27:28 +0200

    Update circle_area.m

--- PEP50/circle_area.m
+++ PEP50/circle_area.m
@@ -14,35 +14,36 @@ function sphere_area = circle_area(hit_pin_xyz, grid_col, grid_row)
     %determine the xyz coordinates of the voxel being hit
     v = hit_pin_xyz(1:3,1);
     hit_pin = hit_pin_xyz(:,2:end);
-    for i=1:size(hit_pin,2)
+    sz = size(hit_pin,2);
+    for i=1:sz
         hit_pin(1:3,i) = hit_pin(1:3,i) - v(1:3); 
     end
     
     %here we calculate phi and theta note, phi is the angle with (x,y)
-    phi = atan( hit_pin(:,2) ./ hit_pin(:,1) );
-    theta = pi/2 - atan( hit_pin(:,3) ./ sqrt( hit_pin(:,1).^2 + hit_pin(:,2).^2) );
+    phi = atan( hit_pin(2,:) ./ hit_pin(1,:) ) + (hit_pin(1,:) < 0) .* (hit_pin(2,:) > 0)*pi/2 - (hit_pin(1,:) < 0) .* (hit_pin(2,:) < 0)*pi/2;
+    theta = atan( hit_pin(3,:) ./ sqrt( hit_pin(1,:).^2 + hit_pin(2,:).^2) );
     
     %now we want to calculate the area on the circle, we do this by
     %calculating the borders. we take 12 as a estimate to make sure we hit
     %all the grids.
     
     %determine the width of the pinholes
-    %d_pin = hit_pin(:,6); 
-    d_pin = ones(size(hit_pin));
-    r_pin = d_pin/2;
+    r_pin = hit_pin(6,:);
+    %but now in angles.
+    r_pin = atan(r_pin) ./ sqrt( hit_pin(1,:).^2 + hit_pin(2,:).^2 + hit_pin(3,:).^2);
     
     %maak het aantal stappen moet overlegd worden
-    steps = max(grid_row,grid_col);
+    steps = max(grid_row,grid_col)*4;
     gamma = linspace(2*pi/steps, 2*pi, steps);
     
     
     %determine the angles of the surface vector
-    %pin_phi = hit_pin(:,4);
-    %pin_theta = hit_pin(:,5);
+    pin_phi = hit_pin(4,:);
+    pin_theta = hit_pin(5,:);
     
     %begin making the area that is been seen by  the pinhole
-    area_border = zeros( length(hit_pin), steps, 2);
-    for i=1:length(hit_pin)
+    area_border = zeros(sz, steps, 2);
+    for i=1:sz
         for j=1:steps
             area_border(i,j,:) = [(phi(i) + cos(gamma(j)).*r_pin(i)) (theta(i) + sin(gamma(j)).*r_pin(i))];
         end
@@ -52,7 +53,16 @@ function sphere_area = circle_area(hit_pin_xyz, grid_col, grid_row)
     
     %row = niet x2
     %grid phi = row
-    area_border(:,:,1) = floor(area_border(:,:,1) / (2 * pi) * grid_row);
-    area_border(:,:,2) = floor(area_border(:,:,2) / (2 * pi) * grid_col);
+    area_border(:,:,1) = round(area_border(:,:,1) / (2 * pi) * grid_row)/grid_row*2*pi;
+    area_border(:,:,2) = round(area_border(:,:,2) / pi * grid_col)/grid_col*pi;
     sphere_area = area_border;
+    %%{
+    figure;
+    hold on;
+    for i=1:sz
+        plot(area_border(i,:,1),area_border(i,:,2));
+    end
+    axis([-pi pi -pi/2 pi/2]);
+    grid on;
+    %%}
 end

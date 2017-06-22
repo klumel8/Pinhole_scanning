23f52e03a36191f215d9690001f9dc9b3ef43a30
koloneltedi <sashaivlev@hotmail.com>
2017-06-22 15:06:07 +0200

    Update Main.m

--- PEP50/Main Versie4/Main.m
+++ PEP50/Main Versie4/Main.m
@@ -12,10 +12,6 @@ end
 quality = zeros(size(voxelCoords,1),1);
 
 disp('Entering the voxelIntersectLoop')
-for i = 1:size(voxelCoords,1)
-    voxelCoor = voxelCoords(i,1:3);
-singleVoxelPinholes = pinhole_scanning_1voxel(translatedScanning, voxelCoor);
-quality(i) = Intersections(gridrow,gridcol,singleVoxelPinholes,gridcir);
-end
+quality = Intersections(gridrow,gridcol,voxelCoords,gridcir,translatedScanning);
 
 end
